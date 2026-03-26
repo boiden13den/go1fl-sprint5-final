0b136462797bceb1338bf27d87a8be61164cbe11
Denis Boyko <boiden13den@gmail.com>
2026-03-26 22:28:46 +0300

    All functions have been written. First attempt to complete the task

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,56 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"personaldata"
+	"spentenergy"
+	"strconv"
+	"strings"
+	"time"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	if datastring == "" || len(datastring) <= 0 {
+		return errors.New("Datastring is empty")
+	}
+	arr := strings.Split(datastring, ",")
+	if len(arr) < 2 {
+		return errors.New("Amount of arguments less 3")
+	}
+	if len(arr) > 2 {
+		return errors.New("Amount of arguments more 3")
+	}
+	steps, err := strconv.Atoi(arr[0])
+	if err != nil {
+		return err
+	}
+	if steps <= 0 {
+		return errors.New("Steps' amount less or equal zero")
+	}
+	ds.Steps = steps
+	duration, err := time.ParseDuration(arr[1])
+	if err != nil {
+		return err
+	}
+	if duration <= 0 {
+		return errors.New("Duration less or equal zero")
+	}
+	ds.Duration = duration
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	spentCalories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", ds.Steps, distance, spentCalories), nil
 }
---
+++ internal/daysteps/go.mod
@@ -0,0 +1,19 @@
+module daysteps
+
+go 1.25.0
+
+replace personaldata => ../personaldata
+
+replace spentenergy => ../spentenergy
+
+require (
+	github.com/stretchr/testify v1.11.1
+	personaldata v0.0.0-00010101000000-000000000000
+	spentenergy v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
